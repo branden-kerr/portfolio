220fa54bf3099c8035817929e5db3dfbf128a3d1
Branden Kerr <brandenkerr7@gmail.com>
2023-08-19 15:43:47 -0400

    initial

---
+++ src/images/index.js
@@ -0,0 +1,15 @@
+import Chat from './portfolioImages/Chat1.jpg';
+import animalDictionary from './portfolioImages/animalDictionary1.jpg';
+import dogFacts from './portfolioImages/dogFacts1.jpg';
+import hotelLanding from './portfolioImages/hotelLanding1.jpg';
+import realtor from './portfolioImages/realtor1.jpg';
+import writerWebsite from './portfolioImages/writerWebsite1.jpg';
+
+export {
+  Chat,
+  animalDictionary,
+  dogFacts,
+  hotelLanding,
+  realtor,
+  writerWebsite
+}
